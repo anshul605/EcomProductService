ad7a241fb0029c1ee07cc904ab50a22aa44f762a
anshul605 <anshulsharma605@gmail.com>
2024-10-13 14:55:22 +0530

    Updated the lombok version and then getter setter works

--- EcomProductService/src/main/java/dev/anshul/EcomProductService/dto/ProductResponseDTO.java
+++ EcomProductService/src/main/java/dev/anshul/EcomProductService/dto/ProductResponseDTO.java
@@ -16,62 +16,6 @@ public class ProductResponseDTO {
     private String category;//if its product class then it should category object here its category name
     private String imageURL;
     private double rating;
-
-    public int getProductID() {
-        return productID;
-    }
-
-    public void setProductID(int productID) {
-        this.productID = productID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
 }
 
 
